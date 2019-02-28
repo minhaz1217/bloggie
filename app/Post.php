02bdadb9ddf730e110353595999fbf24ae3b7c18
MINHAZ-PC\Minhaz <minhaz1217@gmail.com>
2019-02-28 21:43:29 +0600

    Preparing to add signin and signup

--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,11 @@ class Post extends Model
     public function comments(){
         return $this->hasMany(Comment::class);
     }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+
     public function addComment($body){
         // 3rd way
 
